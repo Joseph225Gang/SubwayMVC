41dd1935a4e1a0a898dfa82c64efa96481304623
Joseph Lai <lai225joseph@hotmail.com>
2018-05-02 15:40:23 +0800

    Repository

---
+++ SubWayTest.Test/SubwayMemberRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SubWayTest.Repository;
+using SubWayTest.Domain;
+
+namespace SubWayTest.Test
+{
+    /// <summary>
+    /// Summary description for SubwayMemberRepositoryTest
+    /// </summary>
+    [TestClass]
+    public class SubwayMemberRepositoryTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SubwayMemberRepository subwayMemberRepository = new SubwayMemberRepository();
+            SubwayMember member = new SubwayMember()
+            {
+                Id = Guid.NewGuid(),
+                LastName = "lia1",
+                FirstName = "lia1",
+                BirthDate = DateTime.Now
+            };
+            subwayMemberRepository.Create(member);
+        }
+    }
+}
